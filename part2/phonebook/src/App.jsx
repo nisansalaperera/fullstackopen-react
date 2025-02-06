fe59860286fcfd54243c791842e12d3975412218
nisansala <nisansalasuranjani@gmail.com>
2025-02-06 23:53:32 +0200

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,44 +1,60 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas" ,
+      number: "040-123456",
+    },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNewName = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
 
   const addName = (event) => {
-    event.preventDefault()
-    const nameExists = persons.some(person => person.name === newName);
+    event.preventDefault();
+    const nameExists = persons.some((person) => person.name === newName);
 
     if (nameExists) {
       alert(`${newName} is already added to the phonebook`);
-    return;
+      return;
     }
     const nameObject = {
       name: newName,
-    }
-  
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-  }
-  
+      number: newNumber,
+    };
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
   const Name = ({ persons }) => {
-    return <li>{persons.name}</li>
-  }
+    return <li>{persons.name} {persons.number}</li>;
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input 
+          name:{" "}
+          <input
             value={newName}
             onChange={handleNewName}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={handleNewNumber}
           />
         </div>
         <div>
@@ -46,18 +62,19 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      
+
       <div>
-      <ul>
-         {persons.map((person, index) => 
-          <Name key={index} persons={person} />
-         )}
-      </ul>
-        
+        <ul>
+          {persons.map((person, index) => <Name
+            key={index}
+            persons={person}
+          />)}
+        </ul>
       </div>
+
       ...
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
